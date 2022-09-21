6af532bc8e76b35d589c833e332edfb3c6b66c58
yuvraj singh <singhyuvraj122@gmail.com>
2022-09-21 23:09:27 +0530

    add task and clear all task and remove task function added

--- app.js
+++ app.js
@@ -6,16 +6,23 @@ const clearTaskBtn = document.querySelector(".filterTask .cardBtn");
 
 //all event listener
 addTaskBtn.addEventListener("click",addTask);
+newTaskInput.addEventListener("focus",clrInpt);
+clearTaskBtn.addEventListener("click",delAllTask);
+
 
 //all event handler
 function addTask(e){
-  console.log(document.forms.querySelector("input[type:'submit']"));
+
+  //checking for empty input
   if(newTaskInput.value.length === 0){
-    alert("no input");
+    return;
   }
 
   //create li node for task
   let task = document.createElement("li");
+
+  //adding class to task
+  task.classList.add("task");
 
   //create textnode and store form input value
   let text = document.createTextNode(newTaskInput.value);
@@ -23,6 +30,38 @@ function addTask(e){
   //append textnode to li node
   task.appendChild(text);
 
-  console.log(task);
+  //create remove button
+  let rmBtn = document.createElement("a");
+  rmBtn.innerText = "x";
 
+  //add class to remove button
+  rmBtn.classList.add("removeBtn");
+
+  //adding event listener to x
+  rmBtn.addEventListener("click",rmTask);
+  
+  //append remove button to li
+  task.appendChild(rmBtn);
+
+  //appending task to task container
+  taskListCont.appendChild(task);
+
+}
+
+function clrInpt(e){
+  if(e.target.value.length !== 0){
+    e.target.value = "";
+  }
+}
+
+function rmTask(e){
+  let task = e.target.parentElement;
+  task.remove();
+}
+
+function delAllTask(e){
+  let count = taskListCont.children.length;
+  while(taskListCont.firstElementChild){
+    taskListCont.firstElementChild.remove();
+  }
 }
